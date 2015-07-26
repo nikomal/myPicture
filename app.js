56fdd9c3ae997f2adc2f3d6ecd828b05b01bba4a
王超毅 <wangchaoyi.tk@gmail.com>
2015-07-26 23:21:15 +0800

    scss

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-app.use(multer(
+app.use(
+    multer(
         {
             dest: './uploads/',
             rename: function (fieldname, filename, req, res) {
